c5973647814240a4805cb94f19e8fb30c046bc6a
Arun Shah (Ameeno) <fastah@gmail.com>
2019-02-19 17:35:03 +0000

    Updated Kexts  + ALC Plug Fix (Static after resume) Enjoy

---
+++ EFI/CLOVER/E7440 Fixes/alc_fix_ALC292/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo -e "Installing ALCPlugFix Daemon to /usr/bin"
+sudo cp ALCPlugFix /usr/bin
+sudo chmod 755 /usr/bin/ALCPlugFix
+sudo chown root:wheel /usr/bin/ALCPlugFix
+sudo cp hda-verb /usr/bin
+sudo chmod 755 /usr/bin/hda-verb
+sudo chown root:wheel /usr/bin/hda-verb
+sudo cp good.win.ALCPlugFix.plist /Library/LaunchAgents/
+sudo chmod 644 /Library/LaunchAgents/good.win.ALCPlugFix.plist
+sudo chown root:wheel /Library/LaunchAgents/good.win.ALCPlugFix.plist
+sudo launchctl load /Library/LaunchAgents/good.win.ALCPlugFix.plist
+echo -e "Daemon is installed and loaded. Enjoy!"
+
+exit 0
